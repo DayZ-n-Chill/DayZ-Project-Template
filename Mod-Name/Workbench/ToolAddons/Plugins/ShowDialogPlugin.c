d0eafe444abce2c4a763af9744ef1e8d4c711e3a
DayZ n' Chill <brian@dayznchill.com>
2024-03-22 11:40:41 -0500

    GitIgnore and Minor Updates
    Started figuring out the plugins for DayZ Workbench

---
+++ Mod-Name/Workbench/ToolAddons/Plugins/ShowDialogPlugin.c
@@ -0,0 +1,12 @@
+// class ShowDialogPlugin : WorkbenchPlugin {
+//     void Run() {
+//         Workbench.Dialog("Notification", "Hello, this is your message!");
+//     }
+
+//     void Configure() {
+
+//     }
+// };
+
+// [WorkbenchPluginAttribute("ShowDialog", "Shows a dialog message on Ctrl+N", "Ctrl+N", "", {"ScriptEditor"})]
+// class ShowDialogPluginAttributes {}
--- Servers/Cherno/StartServer.bat
+++ Servers/Cherno/StartServer.bat
@@ -1,11 +1,16 @@
 ::Mods you want to run on the server. 
-SET MODS="P:\Mods\@Colorful-UI;"
+SET "MODS=P:\Mods\@CF;P:\Mods\@Dabs Framework;P:\Mods\@Community-Online-Tools;"
 
 :: Set the directory to your project.  This is where you cloned your . 
 SET "PROJECTDIR=E:\2024 Projects\DayZ Projects\DayZ-Project-Template"
 
 :: Set the Game directory where you have DayZ installed
 SET "GAMEDIR=C:\Program Files (x86)\Steam\steamapps\common\DayZ"
+
+:: Set the Client Diagnostic Log directory 
+SET "CLIENTLOGSDIR=%PROJECTDIR%\Servers\!ClientDiagLogs"
+::NOTE:: !ClientDiagLogs is located in the root of your DayZ installation folder by Default
+:: Relpace "-profiles=!ClientDiagLogs" with  "-profiles=%CLIENTLOGSDIR%" on the last line in the bat file.
 
 :: Set Location of Server Profiles Folder
 SET "PROFILES=%PROJECTDIR%\Servers\Cherno\Profiles"
@@ -20,7 +25,7 @@ SET "MISSIONDIR=%PROJECTDIR%\Missions\Vanilla\dayzOffline.chernarusplus"
 SET "EXPMISSIONDIR=%PROJECTDIR%\Missions\Experimental\dayzOffline.chernarusplus"
 
 :: Start Cherno server.
-start /D "%GAMEDIR%\" DayZDiag_x64.exe -server -noPause -doLogs -mod=%MODS% -profiles=%PROFILES% -battleye=0 -filepatching=1 "-mission=%MISSIONDIR%" "-config=%SERVERCFG%"
+start /D "%GAMEDIR%\" DayZDiag_x64.exe -server -noPause -doLogs "-mod=%MODS%" "-profiles=%PROFILES%" -battleye=0 -filepatching=1 "-mission=%MISSIONDIR%" "-config=%SERVERCFG%"
 
 :: Play on local server.
-start /D "%GAMEDIR%\" DayZDiag_x64.exe -profiles=!ClientDiagLogs -mod=%MODS% -filepatching=1 -connect=127.0.0.1 -port=2302 
+start /D "%GAMEDIR%\" DayZDiag_x64.exe "-profiles=%CLIENTLOGSDIR%" "-mod=%MODS%" -filepatching=1 -connect=127.0.0.1 -port=2302 
